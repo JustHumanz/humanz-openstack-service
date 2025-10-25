b35100f6d94fa27e3abb74a6d8b1c69ed26d77bd
JustHumanz <Just@Humanz.moe>
2025-10-26 03:44:45 +0700

    Fix clouds.yaml&terra plan

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -3,7 +3,7 @@ for dir in $(ls -d */); do
   cd $dir
   echo "⏳ Deploying '$dir' with terraform."  
   terraform init
-  terraform plan -auto-approve
+  terraform plan
   terraform apply -auto-approve
   cd $curr_dir
 done
--- k8s/testing_cluster/variables.tf
+++ k8s/testing_cluster/variables.tf
@@ -1,21 +1,3 @@
-variable "s3_endpoint" {
-  description = "Endpoint URL for the S3-compatible storage"
-  type        = string
-  default     = "http://127.0.0.1:9090"
-}
-
-variable "s3_access_key" {
-  description = "Access key for S3-compatible storage"
-  type        = string
-  default     = ""
-}
-
-variable "s3_secret_key" {
-  description = "Secret key for S3-compatible storage"
-  type        = string
-  default     = ""
-}
-
 variable "os_cloud" {
   description = "Name of the OpenStack cloud configuration to use"
   type        = string
