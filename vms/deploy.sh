3773bea20651f11dd4c073e6dc18863c8f54d6b7
JustHumanz <Just@Humanz.moe>
2025-10-27 21:07:31 +0700

    Fix basename

--- vms/deploy.sh
+++ vms/deploy.sh
@@ -1,8 +1,9 @@
 curr_dir=$pwd
 for dir in $(ls -d */); do
-  cd $dir
-  echo "⏳Deploying vm '$dir' with terraform."  
-  terraform init -backend-config="key=$dir/terraform.tfstate" -reconfigure
+  base_name=$(basename $dir)
+  cd $base_name
+  echo "⏳Deploying vm '$base_name' with terraform."
+  terraform init -backend-config="key=$base_name/test/terraform.tfstate" -reconfigure
   terraform plan
   terraform apply -auto-approve
   cd $curr_dir
